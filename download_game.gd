b5d33c900d7ca848c2330f28c744df3c6cc2cc80
VeryCoolMike <mreznik2011@gmail.com>
2025-08-31 20:39:17 +1000

    eh

--- download_game.gd
+++ download_game.gd
@@ -1,4 +1,4 @@
 extends Button
 
 func _on_pressed() -> void:
-    JavaScriptBridge.eval("window.open('https://github.com/VeryCoolMike/rhythm-paradise/releases/download/build/windows_release.zip', '_blank');")
+    JavaScriptBridge.eval("window.open('https://github.com/VeryCoolMike/rhythm-paradise/releases/download/build/Rhythm_Paradise.exe', '_blank');")
